fa5f2f0d6fe8f899504865bdb83f55c71a619cc4
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2025-07-21 14:38:30 -0400

    Slightly adjust if statement spacing

--- javascript.js
+++ javascript.js
@@ -9,31 +9,31 @@ const operations = {
     subtraction: new Operation("-", (operand1, operand2) => operand1 - operand2),
     multiplication: new Operation("x", (operand1, operand2) => operand1 * operand2),
     division: new Operation("/", (operand1, operand2) => {
-        if(operand2 === "0") return "CAN'T DIVIDE BY 0";
+        if (operand2 === "0") return "CAN'T DIVIDE BY 0";
         else return operand1 / operand2;
     }),
 };
 
-const body = document.querySelector("body");
+const body = document.querySelector("body"); 
 ["click", "keydown"].forEach((eventType) => {
     body.addEventListener(eventType, (event) => {
         let input = event.type === "click" ? 
                         event.target.textContent : 
                         event.key;
 
-        if((input >= "0" && input <= "9") || input === ".") {
+        if ((input >= "0" && input <= "9") || input === ".") {
             let newCharacter = input;
 
             const isNewOperand = operands.length === 0 ||
                                  (operands.length === 1 && operation !== undefined);
 
-            if(isNewOperand) {
-                if(result) {
+            if (isNewOperand) {
+                if (result) {
                     displayText.textContent = "";
                     result = undefined;
                 }
 
-                if(newCharacter === ".") newCharacter = "0" + newCharacter;
+                if (newCharacter === ".") newCharacter = "0" + newCharacter;
 
                 operands.push(newCharacter);
                 displayText.textContent += newCharacter;
@@ -42,32 +42,32 @@ const body = document.querySelector("body");
                                      (newCharacter === "." && !operands.at(-1).includes("."))) &&
                                      operands.at(-1).length < 8;
 
-                if(isValidInput) {
+                if (isValidInput) {
                     operands[operands.length - 1] += newCharacter;
                     displayText.textContent += newCharacter;
                 }
             }
             
-        } else if(input === "DEL" || input === "Backspace") {
-            if(result) {
-                if(result === "CAN'T DIVIDE BY 0") displayText.textContent = "";
+        } else if (input === "DEL" || input === "Backspace") {
+            if (result) {
+                if (result === "CAN'T DIVIDE BY 0") displayText.textContent = "";
                 else operands[0] = result;
                 result = undefined;
             }
 
             const operatorIsBeingDeleted = operands.length === 1 && operation !== undefined;
-            if(operatorIsBeingDeleted) operation = undefined;
+            if (operatorIsBeingDeleted) operation = undefined;
             else if (operands.length !== 0) {
                 operands[operands.length - 1] = operands.at(-1).slice(0, -1);
-                if(operands.at(-1).length === 0) operands.pop();
+                if (operands.at(-1).length === 0) operands.pop();
             }
 
             displayText.textContent = displayText.textContent.slice(0, -1);
             
-        } else if(input === "C") {
+        } else if (input === "C") {
             clear();
             result = undefined;
-            
+
         }
     });
 });
@@ -84,14 +84,14 @@ function clear() {
 }
 
 function getResult() {
-    if(isNaN(operands[0].slice(-1))) operands[0] = operands[0].split("e")[0];
+    if (isNaN(operands[0].slice(-1))) operands[0] = operands[0].split("e")[0];
     
     let result = operation.operate(+operands[0], +operands[1]);
 
-    if(result !== "CAN'T DIVIDE BY 0") {
+    if (result !== "CAN'T DIVIDE BY 0") {
         result = Math.round(result * 100) / 100;
         result = result.toString();
-        if(result.length > 8) result = result.toExponential(2);
+        if (result.length > 8) result = result.toExponential(2);
     }
 
     return result;
